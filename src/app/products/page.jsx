4d3b2db01d042453c1d93449b083d4215818c69f
raniakolachi <raniakolachi.4xwave@gmail.com>
2025-01-10 12:11:57 +0500

    I make some changes

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -12,6 +12,15 @@ const Page = () => {
     return (
         <div>
             <h1 className="bg-green-500 text-white text-3xl font-bold p-4">Products Details Base</h1>
+         <h2 className='ms-4'>
+            Product Titte
+         </h2>
+         <p className='ps-4'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam illo excepturi commodi aperiam obcaecati eum, quos, recusandae magni, eveniet velit maiores accusantium. Illo voluptates cum accusamus fuga? Veritatis, quos dicta.
+         </p>
+          <button className='bg-pink-400 text-white rounded ms-3 my-2 p-2'>
+            Add to Cart
+         </button> 
           
         </div>
     );
